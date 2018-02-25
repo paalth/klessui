f38f36f88ea17649fff7f4e7ffb7b83029ea4c04
Paal Thorstensen <paal@computer.org>
2018-02-24 18:04:37 -0800

    Disable test setup and teardown.

--- klessui/src/test/java/io/kless/klessui/test/BuilderTest.java
+++ klessui/src/test/java/io/kless/klessui/test/BuilderTest.java
@@ -19,26 +19,26 @@ public class BuilderTest {
     private HttpServer server;
     private WebTarget target;
 
-    @Before
-    public void setUp() throws Exception {
-        // start the server
-        server = Main.startServer(Main.DEFAULT_PORT);
-        // create the client
-        Client c = ClientBuilder.newClient();
-
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
-        target = c.target(Main.BASE_URI);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        server.stop();
-    }
+//    @Before
+//    public void setUp() throws Exception {
+//        // start the server
+//        server = Main.startServer(Main.DEFAULT_PORT);
+//        // create the client
+//        Client c = ClientBuilder.newClient();
+//
+//        // uncomment the following line if you want to enable
+//        // support for JSON in the client (you also have to uncomment
+//        // dependency on jersey-media-json module in pom.xml and Main.startServer())
+//        // --
+//        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
+//
+//        target = c.target(Main.BASE_URI);
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//        server.stop();
+//    }
 
     /**
      * Test event handler builder retrieval
